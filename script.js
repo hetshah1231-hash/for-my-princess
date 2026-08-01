0b36f381bcebd19bd66e954b313196c123a2d29a
hetshah1231-hash <hetshah1231@gmail.com>
2026-08-01 21:23:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -165,3 +165,10 @@ Het ❤️
 `;
 
 });
+document.addEventListener("mousemove",(e)=>{
+
+document.body.style.setProperty("--x",e.clientX+"px");
+
+document.body.style.setProperty("--y",e.clientY+"px");
+
+});
